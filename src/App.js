228c3a120a27698c8ad7985236b87d9b87ca82f2
weslwood <weslwood@hotmail.com>
2021-11-24 18:18:05 -0500

    Controlled Forms

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './components/DirectoryComponent';
-import './App.css';
+import React, { Component } from "react";
+import Main from "./components/MainComponent";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
-                </div>
-                </Navbar>
-                <Directory />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
